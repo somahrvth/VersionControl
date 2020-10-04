10f0705b87439385d0f879e1e88c454d03e14df1
somahrvth <horvathsoma12@gmail.com>
2020-10-04 18:49:06 +0200

    excel4

--- UserMaintenance/excelgenerate/excelgenerate/Form1.cs
+++ UserMaintenance/excelgenerate/excelgenerate/Form1.cs
@@ -17,10 +17,47 @@ namespace excelgenerate
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+               
+                xlApp = new Excel.Application();
+
+                
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                
+                xlSheet = xlWB.ActiveSheet;
+
+               
+                //CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void LoadData()
@@ -28,6 +65,7 @@ namespace excelgenerate
             Flats = context.Flats.ToList();
         }
 
+        
 
     }
 }
