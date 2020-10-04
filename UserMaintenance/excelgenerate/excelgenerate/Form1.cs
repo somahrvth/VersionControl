a389f4586f52bedc41a981188471e10bc17ae82c
somahrvth <horvathsoma12@gmail.com>
2020-10-04 19:10:04 +0200

    excel6

--- UserMaintenance/excelgenerate/excelgenerate/Form1.cs
+++ UserMaintenance/excelgenerate/excelgenerate/Form1.cs
@@ -83,7 +83,9 @@ namespace excelgenerate
                     counter++;
                 }
 
-
+                    xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
 
         private void CreateExcel()
