11a7a18e2bd112fc13c7bfe2605d0a1aba95b766
somahrvth <horvathsoma12@gmail.com>
2020-10-04 18:39:06 +0200

    excel2

--- UserMaintenance/excelgenerate/excelgenerate/Form1.cs
+++ UserMaintenance/excelgenerate/excelgenerate/Form1.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace excelgenerate
 {
+    
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
+
     }
 }
