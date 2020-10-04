ab9d9e8ac3aba68718bae4b9f66c3813f8cba3f6
somahrvth <horvathsoma12@gmail.com>
2020-10-04 19:00:11 +0200

    excel5

--- UserMaintenance/excelgenerate/excelgenerate/Form1.cs
+++ UserMaintenance/excelgenerate/excelgenerate/Form1.cs
@@ -26,6 +26,64 @@ namespace excelgenerate
             InitializeComponent();
             LoadData();
             CreateExcel();
+           
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void CreateTable()
+        {
+                string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    xlSheet.Cells[1, i + 1] = headers[i];
+                }
+
+                object[,] values = new object[Flats.Count, headers.Length];
+
+                int counter = 0;
+                foreach (Flat f in Flats)
+                {
+                    values[counter, 0] = f.Code;
+                    values[counter, 1] = f.Vendor;
+                    values[counter, 2] = f.Side;
+                    values[counter, 3] = f.District;
+                    if (f.Elevator)
+                        values[counter, 4] = "van";
+                    else
+                        values[counter, 4] = "nincs";
+                    values[counter, 5] = f.NumberOfRooms;
+                    values[counter, 6] = f.FloorArea;
+                    values[counter, 7] = f.Price;
+                    values[counter, 8] = "=" + GetCell(counter + 2, 8) + "/" + GetCell(counter + 2, 7);
+                    counter++;
+                }
+
+
         }
 
         private void CreateExcel()
@@ -41,10 +99,10 @@ namespace excelgenerate
                 
                 xlSheet = xlWB.ActiveSheet;
 
-               
-                //CreateTable(); 
 
-                
+                CreateTable();
+
+
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
