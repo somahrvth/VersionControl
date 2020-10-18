0b750c3f3d904464496d44cbc0b70e2d986dc84f
somahrvth <horvathsoma12@gmail.com>
2020-10-18 21:31:38 +0200

    commit5

--- UserMaintenance/Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ UserMaintenance/Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Webszolgaltatas.MnbServiceReference;
 
@@ -22,7 +23,28 @@ namespace Webszolgaltatas
             InitializeComponent();
             GetExchangeRates();
             XmlProcess();
+            ChartDataDisplay();
             dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates;
+        }
+
+        private void ChartDataDisplay()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void GetExchangeRates()
